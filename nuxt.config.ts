ff525035d610fa34413c1bc032f36470cf5db916
Boluwatife Johnson <jaycewap@gmail.com>
2025-08-17 22:11:01 +0100

    fix: netlify functions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,17 +6,7 @@ export default defineNuxtConfig({
   ssr: true,
   nitro: {
     preset: 'netlify',
-    // experimental: {
-    //   wasm: false
-    // },
-    // // Remove the plugins line - this was causing the error
-    // externals: {
-    //   inline: ['@prisma/client']
-    // }
   },
-  // build: {
-  //   transpile: ["@prisma/client"],
-  // },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
