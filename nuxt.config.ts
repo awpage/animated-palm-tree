8cbe3fcb04c19afe6ced7a49e5556dc2cb458433
Boluwatife Johnson <jaycewap@gmail.com>
2025-08-23 09:47:57 +0100

    fix: for railway

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   ssr: true,
-  nitro: {
-    preset: "netlify",
-  },
+  // nitro: {
+  //   preset: "netlify",
+  // },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
